451d2150bdc5cea52aac23f1de240ef3b9ac3585
pixel-Teee <1173769234@qq.com>
2023-09-13 23:10:10 +0800

    add win32 window

---
+++ source/Platform/Win32/Win32Window.cpp
@@ -0,0 +1,14 @@
+#include "Win32Window.h"
+
+#include <Windows.h>
+
+namespace ACB {
+
+	Win32Window::Win32Window()
+	{
+
+	}
+	Win32Window::~Win32Window()
+	{
+	}
+}
---
+++ source/Platform/Win32/Win32Window.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Window.h"
+
+namespace ACB {
+	class Win32Window : public Window {
+	public:
+		Win32Window();
+
+		~Win32Window();
+	};
+}
